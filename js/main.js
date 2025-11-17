98e795a9ea929b1ad4375ef733ce6b44d0dc06a7
Chaitali Deore <chaitalideore@gmail.com>
2025-11-17 10:19:13 +0100

    add sidebar js menu

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const sidebar_links = [
+  { text: "Echidna", url: "", id: "echidnaLink" },
+  { text: "Tasmanian Devil", url: "", id: "tasmaniandevilLink" },
+  { text: "Quokka", url: "", id: "quokkaLink" },
+  { text: "Frill-necked lizard", url: "", id: "frill-neckedlizardLink" },
+  { text: "Hawksbill Turtle", url: "", id: "hawksbillturtleLink" },
+  { text: "Perentie", url: "", id: "perentieLink" },
+  { text: "Cassowary", url: "", id: "zassowaryLink" },
+  { text: "Kookaburra", url: "", id: "kookaburraLink" },
+  {
+    text: "Yellow Tailed Black Cockatoo",
+    url: "",
+    id: "yellowtailedblackcockatooLink",
+  },
+];
+let sidebar_list = document.querySelector(".sidebar ul");
+
+sidebar_links.forEach((links) => {
+  const sidebartext = document.createElement("li");
+  const sidebarLink = document.createElement("a");
+  const sidebarId = links.id;
+
+  sidebartext.textContent = links.text;
+  sidebarLink.textContent = links.url;
+
+  sidebartext.appendChild(sidebarLink);
+  sidebar_list.appendChild(sidebartext);
+});
